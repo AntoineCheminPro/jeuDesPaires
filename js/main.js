f5b1ffce98777abac8f924bc55479fb6784c140c
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-09-01 21:03:43 +0200

    first issue functionnal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-let cards=document.getElementsByClassName("bottom");
+const cards=document.getElementsByClassName("bottom");
+const choosenCards=[];
+let colorChoices=[];
+let foundPair =[];
 
-
-function reverse (){
-    classList.add("returned");
+function delayedTurn() {
+    window.setTimeout(slowTurn, 1500);
 }
+
+function slowTurn(){
+    cards[choosenCards[0]].classList.remove("turned")
+    cards[choosenCards[1]].classList.remove("turned")
+    choosenCards.shift(choosenCards[1]);
+    choosenCards.shift(choosenCards[0]);
+}
+
+for (let i=0; i<cards.length;i++){
+    cards[i].addEventListener ("click", function(){
+        cards[i].classList.add("turned");
+        choosenCards.push(i);
+        colorChoices.push(cards[i].value);
+        if (colorChoices.length===2){
+            if (colorChoices[0] === colorChoices[1]){
+                foundPair.push(colorChoices[0]);
+                choosenCards.shift(choosenCards[1]);
+                choosenCards.shift(choosenCards[0]);
+            }
+            else{ 
+                delayedTurn(cards);
+            }
+            colorChoices=[];
+        }
+    })
+};
+
 
