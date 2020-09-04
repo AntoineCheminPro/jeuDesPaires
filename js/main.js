582ea83d4c6b977e5cc803b5ac8b166b7467ca06
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-09-04 20:52:59 +0200

    peppa added

--- js/main.js
+++ js/main.js
@@ -6,14 +6,48 @@ const colorByValue = {
     3:"#FCFCFC",
     4:"#92AFD7",
     5:"#934B00",
-    6:"#FF579F"
+    6:"#FF579F",
+    7:"#9BA2FF",
+    8:"#2A2E45",
+    9:"#E8C547",
+    10:"#CDD1C4",
+    11:"#AF3B6E",
+    12:"#149911",
+    13:"#1EFC1E",
+    14:"#7A542E",
+    15:"#94ECBE",
+    16:"#F4DBD8",
+    17:"#E980FC ",
+    18:"#6B818C",
+    19:"#090C9B",
+    20:"#870058"
+
 }
 
+const imgPeppa ={
+    1:"img/peppapig/peppa_pig_splat-1.png", 
+    2:"img/peppapig/george_pig_splat-1.png",
+    3:"img/peppapig/daddy_pig_splat-1.png",
+    4:"img/peppapig/mummy_pig_splat-1.png",
+    5:"img/peppapig/grandpa_pig_splat-1.png",
+    6:"img/peppapig/granny_pig_splat-1.png",
+    7:"img/peppapig/candy_cat_splat-1.png",
+    8:"img/peppapig/danny_dog_splat-1.png",
+    9:"img/peppapig/emily_elephant_splat-1.png",
+    10:"img/peppapig/freddie_fox_splat-1.png",
+    11:"img/peppapig/gerald_giraffe_splat-1.png",
+    12:"img/peppapig/molly_mole_splat-1.png",
+    13:"img/peppapig/pedro_pony_splat-1.png",
+    14:"img/peppapig/rebecca_rabbit_splat-1.png",
+    15:"img/peppapig/suzy_sheep_splat-1.png",
+    16:"img/peppapig/zoe_zebra_splat-1.png",
+}
 var deck= [];
 
 let colorChoices=[];
 let foundPair =[];
-let deckSize = 12;
+let deckSize = 8;
+var speed = 1200;
 
 // functions
 
@@ -49,11 +83,18 @@ function craftCard(cardAtWork){
     let board = document.getElementById("cardBoard");
     let article = document.createElement("article");
     let button = document.createElement("button");
-    article.classList.add("card", "m-2", "p-0", "col-3", "border-0", "round");
+    article.classList.add("card", "m-2", "p-0","col-3", "col-xl-2","col-lg-2", "col-md-3", "border-0", "round");
     article.style.backgroundColor = colorByValue[cardAtWork];
+    let img = document.createElement("img");
+    let imgBack = document.createElement("img");
+    img.src =imgPeppa[cardAtWork];
+    imgBack.src ="img/peppapig/peppa_george_characters.png";
+    img.classList.add("effect");
     button.setAttribute("href","#");
     button.classList.add("bottom", "round");
     button.setAttribute("value",cardAtWork);
+    button.appendChild(imgBack);
+    article.appendChild(img);
     article.appendChild(button);
     board.appendChild(article);
 }
@@ -63,7 +104,7 @@ function start(){
     bordCleaner();
     deck = deckConstructor();
     let btn = document.getElementById("start");
-    // btn.classList.add("turned");
+    btn.classList.add("turned");
     let cards = drawAll();
     cards = play(cards);
 }
@@ -79,7 +120,7 @@ function delayedTurn(cards) {
     window.setTimeout(function(){
         slowTurn(cards);
     }, 
-    600);
+    speed);
     return cards;
 }
 
@@ -94,19 +135,24 @@ function slowTurn(cards){
 
 // main
 function play(cards){
-    let trys =0;
+    let score =0;
     let inProgress =0;
+    let trys =0;
     for (let i=0; i<cards.length;i++){
         cards[i].addEventListener ("click", function(){
             if (inProgress<2){
                 cards[i].classList.add("turned");
                 choosenCards.push(i);
                 colorChoices.push(cards[i].value);
+
                 inProgress+=1;
                 if (colorChoices.length===2){
                     trys +=1;
                     if (colorChoices[0] === colorChoices[1]){
+                        score +=1;
                         foundPair.push(colorChoices[0]);
+                        // choosenCards[1].classList.add("effect");
+                        // choosenCards[0].classList.add("effect");
                         choosenCards.shift(choosenCards[1]);
                         choosenCards.shift(choosenCards[0]);
                         inProgress = 0;
